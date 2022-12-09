680f676f1e515a1fde7c126c735e7d0a277669ba
Isaac  Tingo <newtone480@gmail.com>
2022-12-09 10:46:05 +0300

    Fix access token

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -31,52 +31,76 @@ class PaymentController extends Controller
 
     public function stkPush(Request $request)
     {
-        Log::info("Sending request");
-
         $fullName = $request->fullName;
         $phoneNumber = $request->phoneNumber;
         $amount = $request->amount;
+        //------------generate access token-----------
 
-        $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-        $business_shortcode = 174379;
-        $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
-        $timestamp = Carbon::rawParse('now')->isoFormat('YYYYMMDDHHmmss');
-        $password = base64_encode($business_shortcode . $passkey . $timestamp);
+        $consumer_key = 'dM1AQniOznQkoFohuPGXowgMALOcUwsr';
+        $consumer_secret = 'l31P1jJLbwhKkHzy';
+        $credentials = base64_encode($consumer_key . ':' . $consumer_secret);
+        $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer CrVlN4LbXSoNPcikGqeFZ9qmoFOp'));
-
-        $curl_post_data = array(
-            "BusinessShortCode" => $business_shortcode,
-            "Password" => $password,
-            "Timestamp" => $timestamp,
-            "TransactionType" => "CustomerPayBillOnline",
-            "Amount" => $amount,
-            "PartyA" => $phoneNumber,
-            "PartyB" => 174379,
-            "PhoneNumber" => $phoneNumber,
-            "CallBackURL" => "https://42e8-105-27-98-86.ngrok.io/api/callback",
-            "AccountReference" => "CompanyXLTD",
-            "TransactionDesc" => "Payment of X"
-        );
-
-        $data_string = json_encode($curl_post_data);
-
+        curl_setopt($curl, CURLOPT_URL, $access_token_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization:Basic ' . $credentials));
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+        $access_token_response = curl_exec($curl);
+        $error = curl_error($curl);
 
-        curl_close($curl);
+        if ($error) {
 
-
-        if ($err) {
-            return $err;
         } else {
-            return $response;
+            $access_token = json_decode($access_token_response);
+            $new_access_token = $access_token->access_token;
+
+            //------------initiate stk push---------------
+
+            $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+            $business_shortcode = 174379;
+            $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+            $timestamp = Carbon::rawParse('now')->isoFormat('YYYYMMDDHHmmss');
+            $password = base64_encode($business_shortcode . $passkey . $timestamp);
+
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $new_access_token));
+
+            $body = array(
+                "BusinessShortCode" => $business_shortcode,
+                "Password" => $password,
+                "Timestamp" => $timestamp,
+                "TransactionType" => "CustomerPayBillOnline",
+                "Amount" => $amount,
+                "PartyA" => $phoneNumber,
+                "PartyB" => 174379,
+                "PhoneNumber" => $phoneNumber,
+                "CallBackURL" => "https://42e8-105-27-98-86.ngrok.io/api/callback",
+                "AccountReference" => "CompanyXLTD",
+                "TransactionDesc" => "Payment of X"
+            );
+
+            $data_string = json_encode($body);
+
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
+
+
+            if ($err) {
+                return $err;
+            } else {
+                return $response;
+            }
+
         }
     }
 
