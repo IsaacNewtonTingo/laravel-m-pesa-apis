02df057385ca55cd5e4528a7cd23deade1a20a63
Isaac  Tingo <newtone480@gmail.com>
2022-12-14 14:07:04 +0300

    Make

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -32,102 +32,89 @@ class PaymentController extends Controller
 
     public function stkPush(Request $request)
     {
-        $fullName = $request->fullName;
-        $phoneNumber = $request->phoneNumber;
-        $amount = $request->amount;
-        //------------generate access token-----------
+        $access_token = $this->generate_access_token();
 
-        $consumer_key = env('MPESA_CONSUMER_KEY');
-        $consumer_secret = env('MPESA_CONSUMER_SECRET');
-        $credentials = base64_encode($consumer_key . ':' . $consumer_secret);
-        $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+        $phone_number = $request->phone_number;
+        $amount = $request->amount;
+
+        //------------initiate stk push---------------
+
+        $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+        $business_shortcode = 174379;
+        $passkey = env('MPESA_PASS_KEY');
+        $timestamp = Carbon::rawParse('now')->isoFormat('YYYYMMDDHHmmss');
+        $password = base64_encode($business_shortcode . $passkey . $timestamp);
 
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $access_token_url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization:Basic ' . $credentials));
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $access_token));
+
+        $body = array(
+            "BusinessShortCode" => $business_shortcode,
+            "Password" => $password,
+            "Timestamp" => $timestamp,
+            "TransactionType" => "CustomerPayBillOnline",
+            "Amount" => $amount,
+            "PartyA" => $phone_number,
+            "PartyB" => 174379,
+            "PhoneNumber" => $phone_number,
+            "CallBackURL" => "https://00f5-105-27-98-86.ngrok.io/api/mpesa-response",
+            "AccountReference" => "Jubilee Life Insurance",
+            "TransactionDesc" => "Insurance cover payment"
+        );
+
+        $data_string = json_encode($body);
+
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 
-        $access_token_response = curl_exec($curl);
-        $error = curl_error($curl);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
 
-        if ($error) {
-            return "An error occured";
+        $new_response = json_decode($response);
+
+
+        curl_close($curl);
+
+
+        if ($err) {
+            return $err;
         } else {
-            $access_token = json_decode($access_token_response);
-            $new_access_token = $access_token->access_token;
-
-            //------------initiate stk push---------------
-
-            $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-            $business_shortcode = 174379;
-            $passkey = env('MPESA_PASS_KEY');
-            $timestamp = Carbon::rawParse('now')->isoFormat('YYYYMMDDHHmmss');
-            $password = base64_encode($business_shortcode . $passkey . $timestamp);
-
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $new_access_token));
-
-            $body = array(
-                "BusinessShortCode" => $business_shortcode,
-                "Password" => $password,
-                "Timestamp" => $timestamp,
-                "TransactionType" => "CustomerPayBillOnline",
-                "Amount" => $amount,
-                "PartyA" => $phoneNumber,
-                "PartyB" => 174379,
-                "PhoneNumber" => $phoneNumber,
-                "CallBackURL" => "https://5550-105-27-98-86.ngrok.io/api/callback",
-                "AccountReference" => "CompanyXLTD",
-                "TransactionDesc" => "Payment of X"
-            );
-
-            $data_string = json_encode($body);
-
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-
-            // $decoded_response = json_decode($response);
-
-            // $CheckoutRequestID = $decoded_response->CheckoutRequestID;
-
-            curl_close($curl);
+            Log::info('----------stk push successfull----------');
 
 
-            if ($err) {
-                return $err;
-            } else {
-                return $response;
-            }
+            // $checkout_request_ID = $new_response->CheckoutRequestID;
+            // $merchant_request_ID = $new_response->MerchantRequestID;
+
+            // $pending_payment = new PendingPayments;
+
+            // $pending_payment->phone_number = $phone_number;
+            // $pending_payment->amount = $request->amount;
+            // $pending_payment->user_id = $request->user_id;
+
+            // $pending_payment->checkout_request_ID = $checkout_request_ID;
+            // $pending_payment->merchant_request_ID = $merchant_request_ID;
+
+            // $pending_payment->save();
+
+
+            return $new_response;
+
         }
     }
 
     public function callback(Request $request)
     {
-        Log::info('------------data here-------------');
-        // $body = $request->Body;
+        if ($request->Body['stkCallback']['ResultCode'] === 0) {
+            //successfull payment 
+            $phone_number = $request->Body['stkCallback']['CheckoutRequestID']['CallbackMetadata']['Item'][4]['Value'];
+            Log::info($phone_number);
+        } else {
+            //unsuccessfull payment
+            Log::info("---Payment wasn't successfull---");
 
-        // if ($body->stkCallback->ResultCode === 0) {
-
-        //     $store_data = $body->only([
-        //         'checkoutRequestID' => $body->stkCallback->CheckoutRequestID,
-        //         'phoneNumber' => $body->stkCallback->CallbackMetadata->Item[3]->Value,
-        //         'amount' => $body->stkCallback->CallbackMetadata->Item[0]->Value,
-        //         'mpesaCode' => $body->stkCallback->CallbackMetadata->Item[1]->Value,
-        //     ]);
-
-        //     $new_payment = Payment::create($store_data);
-
-        //     Log::info($new_payment);
-        // } else {
-        //     Log::info($body->stkCallback->ResultDesc);
-        // }
+        }
     }
 
     public function checkPayment(Request $request)
